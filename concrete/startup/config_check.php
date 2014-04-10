b339fb0c7584ff81792d58bd2987447ac59e9a24
Stefan Fodor <stefan@hammerti.me>
2014-04-10 14:54:18 +0200

    removing admin recovery key on first run

--- concrete/startup/config_check.php
+++ concrete/startup/config_check.php
@@ -10,6 +10,13 @@ if (!defined('CONFIG_FILE')) {
 	define('CONFIG_FILE', DIR_CONFIG_SITE . '/site.php');
 }
 
+//remove admin recovery key if exists
+if( file_exists(DIR_CONFIG_SITE . '/recovery/recovery_key.rsa') ) {
+	if( unlink(DIR_CONFIG_SITE . '/recovery/recovery_key.rsa') === false ) {
+		throw new Exception("Could not remove recovery key. Please do it manually!");
+	}
+}
+
 if (file_exists(CONFIG_FILE)) {
 	include(CONFIG_FILE);
 } else {
