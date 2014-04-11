edeb5ec05561a821123c9edc3fb79cda3fbc807a
Stefan Fodor <stefan@hammerti.me>
2014-04-11 13:26:57 +0200

    mde recovery page public available

--- config/install/packages/passwordx/controller.php
+++ config/install/packages/passwordx/controller.php
@@ -176,6 +176,12 @@ class PasswordxStartingPointPackage extends StartingPointPackage {
 		$home->assignPermissions($registered_group, array('view_page'));
 		$home->assignPermissions($admin_group, array('view_page_versions', 'view_page_in_sitemap', 'preview_page_as_user', 'edit_page_properties', 'edit_page_contents', 'edit_page_speed_settings', 'edit_page_theme', 'edit_page_type', 'edit_page_permissions', 'delete_page', 'delete_page_versions', 'approve_page_versions', 'add_subpage', 'move_or_copy_page', 'schedule_page_contents_guest_access'));
 		
+		//recovery page is publicly visible
+		$recovery_page = Page::getByPath('/recovery');
+		$recovery_page->clearPagePermissions();
+		$recovery_page->assignPermissions($guest_group, array('view_page'));
+		$recovery_page->assignPermissions($registered_group, array('view_page'));
+		$recovery_page->assignPermissions($admin_group, array('view_page_versions', 'view_page_in_sitemap', 'preview_page_as_user', 'edit_page_properties', 'edit_page_contents', 'edit_page_speed_settings', 'edit_page_theme', 'edit_page_type', 'edit_page_permissions', 'delete_page', 'delete_page_versions', 'approve_page_versions', 'add_subpage', 'move_or_copy_page', 'schedule_page_contents_guest_access'));
 		
 		//everything under My Passwords should be visible to registered users
 		$my_pass = Page::getByPath('/my-passwords');
