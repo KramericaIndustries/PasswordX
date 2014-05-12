0b9a293aef4005753625ea9175dbcf674f9e7bf6
Stefan Fodor <stefan@hammerti.me>
2014-05-12 15:21:49 +0200

    Encrypting by default in password designer 	modified:   blocks/.gitignore 	deleted:    blocks/encrypted_fancy_shit_rrrr/add.php 	deleted:    blocks/encrypted_fancy_shit_rrrr/auto.js 	deleted:    blocks/encrypted_fancy_shit_rrrr/controller.php 	deleted:    blocks/encrypted_fancy_shit_rrrr/db.xml 	deleted:    blocks/encrypted_fancy_shit_rrrr/edit.php 	deleted:    blocks/encrypted_fancy_shit_rrrr/icon.png 	deleted:    blocks/encrypted_fancy_shit_rrrr/view.php

--- blocks/encrypted_fancy_shit_rrrr/auto.js
+++
@@ -1,7 +0,0 @@
-ccmValidateBlockForm = function() {
-	
-
-	return false;
-}
-
-$(function(){$(".sugest_pass").click(function(){$("#"+$(this).data("target")).val(sanePassword());return false});$(".clear_view").mousedown(function(){$("#miror_"+$(this).data("target")).val($("#"+$(this).data("target")).val());$("#"+$(this).data("target")).css("display","none");$("#miror_"+$(this).data("target")).css("display","inline");return false});$(".clear_view").mouseup(function(){$("#"+$(this).data("target")).css("display","inline");$("#miror_"+$(this).data("target")).css("display","none");return false});$(".clear_view").click(function(){return false})})
